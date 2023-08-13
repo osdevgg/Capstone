f65f0bd92d9531ba9c827effb29e7caf4caa9afe
OS <os@idmail.com.ar>
2023-08-13 11:54:14 -0700

    Models setup. Some data entered and checked in Mysql LittleLemon DB in restaurant_menu and restaurant_booking tables.

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+#    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+#    no_of_guests = models.DecimalField(max_digits=6, decimal_places=0)
+    no_of_guests = models.SmallIntegerField()
+    bookingdate = models.DateTimeField()
+
+class Menu(models.Model):
+#    id = models.SmallIntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+#    inventory = models.IntegerField(max_length=5)
+    inventory = models.SmallIntegerField()
