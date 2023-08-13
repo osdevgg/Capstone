daf651fcd92bf6284a4fd9074c49b68bd6edab89
OS <os@idmail.com.ar>
2023-08-13 13:45:00 -0700

    Started API w DefaultRouter in project urls.py and ModelViewSet views for Booking, and generic views for Menu dispatched from app urls.py. Swapped restaurant and rest_framework entries in APPS list in settings.py \nEntered new bookings and menu from the HTML interface

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import index 
+from .views import index, MenuItemView, SingleMenuItemView
+
   
 urlpatterns = [ 
     # path('', sayHello, name='sayHello'), 
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('menu/', MenuItemView.as_view()),
+    path('menu/<int:pk>', SingleMenuItemView.as_view()),
 ]
