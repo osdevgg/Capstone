eee20da0e47ca9a5f82ba0c289d157d0384ea9e2
OS <os@idmail.com.ar>
2023-08-13 19:20:53 -0700

    Added authentication requirement for Booking URL (restaurant/booking/tables)

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin 
 from django.urls import path 
 from .views import index, MenuItemView, SingleMenuItemView
-
+from rest_framework.authtoken.views import obtain_auth_token
   
 urlpatterns = [ 
     # path('', sayHello, name='sayHello'), 
     path('', index, name='index'),
     path('menu/', MenuItemView.as_view()),
     path('menu/<int:pk>', SingleMenuItemView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
