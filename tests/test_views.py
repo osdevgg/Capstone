0436cbeb09b98658d48816c45e59368d9265aaee
OS <os@idmail.com.ar>
2023-08-15 22:19:07 -0700

    2 problems trying to test the serialization as suggested in the proposed exercise to test the view in test_views.py:
    1. The 2nd use of client instance produces a 404-code response. Even using
    client as a class variable, or scoping it within each method, even with a
    separate instance, or timeout. I've altered the order in which methods
    are called by altering their name (apparently they are called in
    alphabetical order))
    
    2. I have not managed to obtain the 'price' value in 2 decimal figures (when
    the unit cents figure is 0, it cuts to the only significative digit --> 17.3
    instead of 17.30)

--- tests/test_views.py
+++ tests/test_views.py
@@ -5,6 +5,9 @@ from restaurant.views import MenuItemView
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.urls import reverse
+from collections import OrderedDict
+from decimal import Decimal, ROUND_HALF_UP
+import time
 
 client = APIClient()
 
@@ -12,6 +15,7 @@ class MenuViewTest(TestCase):
 
     # def __init__(self):
     #     carte = None
+
 
     carte = [ 
                 ['Bruschetta', 4.50, 27, ],
@@ -22,34 +26,57 @@ class MenuViewTest(TestCase):
     ]
 
     def setUp(self):
+        Menu.objects.raw('delete from test.restaurant_menu;')
         [ Menu.objects.create(title=dish[0], price=dish[1], inventory=dish[2]) for dish in MenuViewTest.carte ]
 
-    def test_get_item(self):
-        item_id = 2
-        i = int(item_id) - 1
-        d = MenuViewTest.carte
-        url = reverse('singlemenuview', args = [item_id])
-#        url = reverse('singlemenuview',args = [3])
-#        url = reverse('singlemenuview',args = [3])
-#        url = '/restaurant/menu/3'
-        print("URL: ", url)
-        response = client.get(url)
-#        response = client.get(reverse('singlemenuview', args = [item_id]))
-        print(response.status_code)
-        print(response.data)
-        item = { 'id' : i, 'title': d[i][0], 'price': d[i][1], 'inventory': d[i][2] }
-#        self.assertDictEqual(response.data,MenuViewTest.carte[1])
-        self.assertDictEqual(response.data,item)
 
     def test_get_all(self):
-        url = reverse('menuview')
-        print(url)
 
-        response = client.get(url)
+        url = reverse('menuview')
+#        print(url)
+
+        # response = client.get(url)
+        response = self.client.get(url)
 #        response = client.get(reverse('menuview'))
         print(response.data)
-        print(response.status_code)
+#        print(response.status_code)
         self.assertEqual(len(response.data), len(MenuViewTest.carte))
 #        self.assertEqual(len(response.data), 5)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_aretrieve_item(self):
+
+#        time.sleep(10) 
+        item_id = 3
+        i = item_id - 2
+#       i = int(item_id)
+        d = MenuViewTest.carte
+        # url = reverse('singlemenuview', args = [item_id])
+        url = reverse('singlemenuview', kwargs={'pk': item_id})
+#        url = reverse('singlemenuview',args = [3])
+#        url = reverse('singlemenuview',args = [3])
+#        url = '/restaurant/menu/3'
+#      #  print("URL: ", url)
+        response = self.client.get(url)
+#        response = client.get(reverse('singlemenuview', args = [item_id]))
+#        print(response.status_code)
+#        print(response.data)
+#        item = { 'id' : i, 'title': d[i][0], 'price': d[i][1], 'inventory': d[i][2] }
+#        price = round(d[i][1],2)
+#        price = Decimal(round(d[i][1],2))
+        price = Decimal(d[i][1])
+        pricedec = price.quantize(Decimal('0.00'),rounding = ROUND_HALF_UP)
+#        print(pricedec)
+        item = { 'title': d[i][0], 'price': pricedec, 'inventory': d[i][2] }
+#        item = { 'title': d[i][0], 'price': Decimal(d[i][1]), 'inventory': d[i][2] }
+#        item = { 'title': d[i][0], 'inventory': d[i][2] }
+#        self.assertDictEqual(response.data,MenuViewTest.carte[1])
+        response_data = response.data.copy()
+        response_data.pop('id', None)
+#        response_data.pop('price', None)
+#        self.assertDictEqual(response.data,item)
+#        self.assertEqual(response.status_code,status.HTTP_200_OK)
+#        self.assertDictEqual(response_data,item)
+        self.assertDictEqual(OrderedDict(response_data),OrderedDict(item))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
 
